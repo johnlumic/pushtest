42a91e2849fc06e82ad6a13d130dbad5545940f3
johnlumic <36335225+johnlumic@users.noreply.github.com>
2025-06-06 10:24:59 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.pushwoosh.com/webpush/v3/pushwoosh-service-worker.js' + self.location.search);
+<script type="text/javascript" src="//cdn.pushwoosh.com/webpush/v3/pushwoosh-web-notifications.js" async></script>
